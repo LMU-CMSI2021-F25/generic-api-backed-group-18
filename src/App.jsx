45b87787e207f57884864a94815c4ab659b328f4
Kaitlin Murphy <kmurph58@lion.lmu.edu>
2025-09-30 13:16:26 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React 101</h1>
+      <h1>Rick and Morty</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
