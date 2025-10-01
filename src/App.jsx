8de04ca2e9f006e2c58f8c55e47325d1b6da9568
Kaitlin Murphy <kmurph58@lion.lmu.edu>
2025-10-01 14:03:04 -0700

    menu button expands to characters, episodes, and locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menuOpen, setMenuOpen] = useState(false)
+  const[data, setData] = useState([])
+  const[category, setCategory] = useState(null)
+
+  useEffect(() => {
+    if (!category) return
+    let endpoint = ""
+    if(category == "characters") endpoint = "https://rickandmortyapi.com/api/character"
+    if (category == "locations") endpoint = "https://rickandmortyapi.com/api/location"
+    if (category == "episodes") endpoint = "https://rickandmortyapi.com/api/episode"
+  }, [category])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Rick and Morty</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setMenuOpen(!menuOpen)}>
           Menu
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-          Use this for Rick and Morty API <code>https://rickandmortyapi.com/api</code>
-        </p>
+        {menuOpen && (
+          <div className="menu">
+            <button onClick={()=>setCategory("characters")}>Characters</button>
+            <button onClick={() => setCategory("locations")}>Locations</button>
+            <button onClick={() => setCategory("episodes")}>Episodes</button>
+          </div>
+        )}
       </div>
       <p className="read-the-docs">
         Group 18 API
